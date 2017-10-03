c0b5b03dd178636b62ff407364434d9963190c9f
M C <machenmusik@gmail.com>
2017-10-03 01:49:22 -0400

    always create this.audioEl even if there's already a scene listener (e.g. sources after the first)

--- src/components/networked-audio-source.js
+++ src/components/networked-audio-source.js
@@ -53,26 +53,12 @@ AFRAME.registerComponent('networked-audio-source', {
       sceneEl.addEventListener('camera-set-active', function (evt) {
         evt.detail.cameraEl.getObject3D('camera').add(sceneEl.audioListener);
       });
-
-      this.audioEl = document.createElement('audio');
-      sceneEl.appendChild(this.audioEl);
-      
-      // for mobile Chrome (and perhaps others), resume audio on user gesture
-      window.addEventListener('touchstart', function() {
-        console.log('touchstart, ' + sceneEl.audioListener.context.state);
-        sceneEl.audioListener.context.resume();
-      });
-      window.addEventListener('touchend', function() {
-        console.log('touchend, ' + sceneEl.audioListener.context.state);
-        sceneEl.audioListener.context.resume();
-      });
-      window.addEventListener('click', function() {
-        console.log('click, ' + sceneEl.audioListener.context.state);
-        sceneEl.audioListener.context.resume();
-      });
     }
     this.listener = sceneEl.audioListener;
 
+
+    this.audioEl = document.createElement('audio');
+    sceneEl.appendChild(this.audioEl);
     this.sound = this.data.positional ? new THREE.PositionalAudio(this.listener) : new THREE.Audio(this.listener);
     el.setObject3D(this.attrName, this.sound);
   }
